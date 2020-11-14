54767c35cdf309a7ecb2c7b143bdea8c26005b28
sarahmansfield <sbmansfield1@gmail.com>
2020-11-14 00:45:56 -0600

    added new function for obtaining direction steps

--- mapdirections.R
+++ mapdirections.R
@@ -1,4 +1,4 @@
-# function to return a leaflet map showing the travel route
+# function to return geoJSON object containing route info
 get_route <- function(startpoint, park) {
   x <- ors_geocode(startpoint, boundary.country = "US")
   
@@ -16,14 +16,25 @@ get_route <- function(startpoint, park) {
     select(latitude) %>%
     pull()
   
-  # map
   coordinates <- list(c(input_long, input_lat), c(park_long, park_lat))
   radius = c(10000, 10000)
   directions <- ors_directions(coordinates, radiuses = radius)
+  directions
+}
+
+# function to return step by step directions (tibble)
+get_steps <- function(directions) {
+  steps <- directions$features[[1]]$properties$segments[[1]]$steps
   
-  leaflet() %>%
-    addTiles() %>%
-    addGeoJSON(directions, fill = FALSE) %>%
-    fitBBox(directions$bbox) %>%
-    addMarkers(directions, lng = park_long, lat = park_lat)
+  distance <- round(map_dbl(steps, `[[`, "distance") / 1609, 2) 
+  duration <- round(map_dbl(steps, `[[`, "duration") / 60, 2)
+  instruction <- map_chr(steps, `[[`, "instruction")
+  name <- map_chr(steps, `[[`, "name")
+  
+  directions.df <- tibble(instruction, distance, duration, name) %>%
+    rename(Direction = instruction, 
+           `Distance (mi)` = distance,
+           `Duration (min)` = duration,
+           Road = name)
+  directions.df
 }
